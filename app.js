07cbcf0fc1caa2371c6c762a4890a7ea1a36528e
Muhammad-Sami-135 <muhammadsamialam8@gmail.com>
2024-09-29 10:59:41 -0700

    work

---
+++ app.js
@@ -0,0 +1,248 @@
+// (Word File Start)
+
+// Chapter no 1 (Alert)
+
+// Q1
+
+alert("firstname = Muhammad Sami");
+alert("Lastname = Jawed");
+alert("email = muhammadsamialam8@gmail.com");
+alert("Phone = 0320-2831715");
+
+// end
+
+// Q2
+
+alert("You're are lerning Javascript!");
+
+// end
+
+// Q3
+
+alert("Welcome to the JavaScript tutorial!");
+
+// end
+
+// Chapter no 2 (Variables for string)
+
+// Q1
+
+var userAge = 18;
+
+// end
+
+// Q2
+
+var my_variable;
+var my_variable = "Hello";
+
+// end
+
+// Q3
+
+var teamName;
+var teamName = "Salam";
+alert(teamName);
+
+// end
+
+// Q4
+
+teamName = "Pakistan";
+
+// end
+
+// Chapter no 3 (Variables for numbers)
+
+// Q1
+
+var caseQty;
+
+// end
+
+// Q2
+
+caseQty = 144;
+
+// end
+
+// (Word File End)
+
+
+// Chapter no 1 (Alert)
+
+// Q1
+
+alert();
+
+// end
+
+// Q2
+
+alert("Q2 Welcome User");
+
+// end
+
+// Q3
+
+alert("Q3 Welcome User\nMuhammad Sami");
+
+// end
+
+// Q4
+
+alert("Q4 Welcome User");
+alert("Q4 Welcome User\nMuhammad Sami");
+
+// end
+
+// Q5
+
+alert("Q5 Muhammad Sami");
+
+// end
+
+
+// Chapter no 2 (VARIABLES FOR STRINGS)
+
+// Q1
+
+var username;
+
+// end
+
+// Q2
+
+var myname = "Muhammad Sami";
+
+// end
+
+// Q3
+
+// a 
+var helloworld;
+
+// b
+helloworld = "chap no 2 Q3 Muhammad Sami";
+
+// c
+alert(helloworld);
+
+// end
+
+// Q4
+
+var firstName = "Muhammad Sami";
+alert(firstName);
+
+var Age = "18 year old";
+alert(Age);
+
+var City = "Karachi";
+alert(City);
+
+// end
+
+// Q5
+
+var Pizza = "Pizza\nPizz\nPiz\nPi\nP";
+alert(Pizza);
+
+// end
+
+// Q6
+
+var email = "muhammadsamialam8@gmail.com";
+alert(email);
+
+// end
+
+// Q7
+
+var book = "A smarter way to learn JavaScript";
+alert(book);
+
+// end
+
+// Q8
+
+var text = "My name is muhammad sami <br>";
+document.write(text);
+
+// end
+
+// Q9
+
+var line = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+alert(line);
+
+// end
+
+
+// Chapter no 3 (VARIABLES FOR NUMBERS)
+
+// Q1
+
+var age = 18;
+alert("I am" + " " + age + " " + "year old");
+
+// end
+
+// Q2
+
+var birthyear = 4;
+alert("You have visit this site" + " " + birthyear + " " + "times");
+
+// end
+
+// Q3
+
+var birth = 2004
+document.write("My birth year is " + birth + "<br> Data type of my declare <br>");
+
+// end
+
+// Q4
+
+var visitorName = "Muhammad Sami";
+var productTitle = "T-shirt";
+var quantity = 5;
+document.write(visitorName + " ordered " + quantity + " " + productTitle + "(s) on XYZ Clothing store.");
+
+// end
+
+
+// Chapter no 4 (VARIABLE NAMES: LEGAL & ILLEGAL)
+
+// Q1
+
+var a = 1 , b = 2 , c = 3;
+
+// end
+
+// Q2
+
+// (legal)
+// var aa = 11;
+// var ab = 12;
+// var ac = 13;
+// var ad = 14;
+// var ae = 15;
+
+// (illegal)
+// var 123aa = 11;
+// var @ab = 12;
+// var ac* = 13;
+// var a^d = 14;
+// var -ae = 15;
+
+// end 
+
+// Q3
+
+var super_star = "Rules for naming JS variables";
+
+// end
+
+
+
